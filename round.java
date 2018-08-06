ad7b0809f94d1a3496d18698de790e8d2571e110
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-08-06 11:43:25 +0530

    Create round.java

---
+++ round.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		double n=16.4d;
+		System.out.print((int)Math.ceil(n));
+	}
+}
