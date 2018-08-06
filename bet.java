9b3fa2a39ba3e894eb1c261074805ab33a10913a
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-08-06 09:23:39 +0530

    Create bet.java

---
+++ bet.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=3,a=2,b=20202;
+		if(a<n&&b>n)
+		System.out.print("Yes");
+		else
+		System.out.print("No");
+	}
+}
